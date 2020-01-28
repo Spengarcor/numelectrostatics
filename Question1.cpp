7572f7be108279453cde48bbddeccc8a359ca2d3
pd-9999 <51860817+pd-9999@users.noreply.github.com>
2020-01-28 15:19:57 +0000

    Q1 partly solved

--- Question1.cpp
+++ Question1.cpp
@@ -3,9 +3,37 @@
 #include <iostream>
 #include <unordered_set>
 #include <string>
+#include <fstream>
 #include "relax.h"
+#include "circle.h"
 
 using namespace std;
+
+
+
+void save_to_csv(vector<vector<double>> mesh){
+
+    ofstream csv_file;
+
+    csv_file.open("for_np.csv");
+
+    for(int i = 0; i != mesh.size(); ++i){
+        for(int j = 0; j != mesh.size(); ++j){
+    
+            csv_file << mesh[i][j];
+            if(j != mesh.size()-1){
+                csv_file << ";";
+            }
+
+        }
+        csv_file << "\n";
+    }
+
+    csv_file.close();
+
+}
+
+
 
 
 
@@ -14,47 +42,31 @@ int main(){
 
 
 
-    //use Grid object to draw shapes (initialise)
-
-
-    
-
     int rows = 100, cols = 100;
 
-    vector<vector<double>> grid(rows, vector<double>(cols));
-    vector<vector<bool>> change_check(rows, vector<bool>(cols, true)); 
+    //use Grid object to draw shapes (initialise)
+    Grid initialise(rows, cols);
 
+    initialise.circle(50,50,10, 10,10,0, true,true,false);
+
+    vector<vector<double>> grid = initialise.get_mesh();
+    vector<vector<bool>> change_check = initialise.get_change_indices();
+
+    save_to_csv(grid);
     
 
-    //initialise array -> change to calling draw methods using draw object
-    for(int i = 0; i != rows; ++i){
-        for(int j = 0; j != cols; ++j){
-            //example 3.3 Griffiths
-
-            if(j == 0){
-                
-                grid[i][j] = 10; //V0 = 10V
-                change_check[i][j] = false;
-
-            } else if(i == 0 || i == rows - 1){
-
-                grid[i][j] = 0;
-                change_check[i][j] = false;
-
-            } else grid[i][j] = 0;
-
-
-        }
-    }
 
 
 
-    Relax solver(grid, change_check);
-
-    double bestp = solver.getBestp();
 
 
-    solver.relaxPotential(bestp, 0.01, 1000);
+
+    //Relax solver(grid, change_check);
+
+    //double bestp = solver.getBestp();
+
+
+    //solver.relaxPotential(bestp, 0.01, 1000);
 
 
 
@@ -63,3 +75,32 @@ int main(){
 
     return 0;
 }
+
+
+
+
+    // vector<vector<double>> grid(rows, vector<double>(cols));
+    // vector<vector<bool>> change_check(rows, vector<bool>(cols, true)); 
+
+    
+
+    // //initialise array -> change to calling draw methods using draw object
+    // for(int i = 0; i != rows; ++i){
+    //     for(int j = 0; j != cols; ++j){
+    //         //example 3.3 Griffiths
+
+    //         if(j == 0){
+                
+    //             grid[i][j] = 10; //V0 = 10V
+    //             change_check[i][j] = false;
+
+    //         } else if(i == 0 || i == rows - 1){
+
+    //             grid[i][j] = 0;
+    //             change_check[i][j] = false;
+
+    //         } else grid[i][j] = 0;
+
+
+    //     }
+    // }
