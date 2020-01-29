b7c8b58c2fc000b46e89beaa850c04c2e09f8857
Spengarcor <2324008m@student.gla.ac.uk>
2020-01-29 09:07:04 +0000

    Matplotlib scalar field

---
+++ visualisation.py
@@ -0,0 +1,28 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap, LinearSegmentedColormap
+from colour import Color
+import matplotlib.colors as colors
+import matplotlib.cm as cm
+
+
+filename = input("Enter a csv file name or path:\n")
+
+data = np.loadtxt(filename, dtype=str, delimiter=' ')
+data = np.delete(data, -1, axis=1)
+data = data.astype(np.float)
+print(data)
+
+#min = min(list(map(min, data)))
+#data = data - min
+
+print(data)
+X = np.linspace(0, len(data[0,:]), len(data[0,:]))
+Y = np.linspace(0, len(data[:,0]), len(data[:,0]))
+values = np.array(data)
+print(values)
+print(X)
+print(Y)
+plt.pcolormesh(X, Y, values, cmap=plt.get_cmap("bwr"))
+
+plt.show()
