edc0b96220fb3c2b98f24ee2edbefdf2628772a3
Spengarcor <2324008m@student.gla.ac.uk>
2020-01-30 15:36:52 +0000

    A tidy version of the scalar field visualisation

--- visualisation.py
+++ visualisation.py
@@ -5,27 +5,21 @@ from colour import Color
 import matplotlib.colors as colors
 import matplotlib.cm as cm
 
-
+# Reads from a specified csv file to a matrix
 filename = input("Enter a csv file name or path:\n")
-
 data = np.loadtxt(filename, dtype=str, delimiter=';')
 data = np.delete(data, -1, axis=1)
 data = data.astype(np.float)
-print(data)
+values = np.array(data)
 
-#min = min(list(map(min, data)))
-#data = data - min
 
-print(data)
+# Establishes the 2D grid to display
 X = np.linspace(0, len(data[0,:]), len(data[0,:]))
 Y = np.linspace(0, len(data[:,0]), len(data[:,0]))
-values = np.array(data)
-print(values)
-print(X)
-print(Y)
 
-def scalar_field(X, Y, values):
+
+def show_scalar_field(X, Y, values):
     plt.pcolormesh(X, Y, values, cmap=plt.get_cmap("bwr"))
     plt.show()
 
-scalar_field(X, Y, values)
+show_scalar_field(X, Y, values)
